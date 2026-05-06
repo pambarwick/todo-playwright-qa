e0dc937fc7e0d4acddf31f78c626753945d8daea
Pamela Hanekom <pam@MacBookPro.home.local>
2026-05-06 17:38:09 -0400

    Add cross-browser testing for Firefox and WebKit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: 'tests',
@@ -12,6 +12,15 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
     },
   ],
 });
